0ba044e92a0a252790df818dcbac8bf5c1a102f0
dylandeloia1 <ddeloia@uncc.edu>
2023-01-22 22:18:26 -0500

    Fixed first line

--- script_1.py
+++ script_1.py
@@ -1,4 +1,4 @@
-# Dylan D'Eloia / intro_python_exercises / itsc3155 / 1 - 13 - 2023
+# Dylan D'Eloia / intermediate_python_exercises / itsc3155 / 1 - 22 - 2023
 
 def unique_list(lst):
     new_list = list(set(lst))
@@ -6,4 +6,3 @@ def unique_list(lst):
 
 my_list = [1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 8]
 print(unique_list(my_list))
-
