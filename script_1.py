ce77d473a905a23fe5781ee549043541f43811e5
dylandeloia1 <ddeloia@uncc.edu>
2023-01-22 22:29:58 -0500

    Fixed Script 1

--- script_1.py
+++ script_1.py
@@ -1,8 +1,9 @@
 # Dylan D'Eloia / intermediate_python_exercises / itsc3155 / 1 - 22 - 2023
 
-def unique_list(lst):
+def get_unique_list(lst):
     new_list = list(set(lst))
     return new_list
 
-my_list = [1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 8]
-print(unique_list(my_list))
+my_list = [1, 2, 3, 2, 1, 4]
+unique_list = get_unique_list(my_list)
+print(unique_list)
